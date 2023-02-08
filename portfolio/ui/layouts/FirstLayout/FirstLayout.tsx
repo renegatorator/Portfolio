04b5c9afb9848256e2efdc0b8cd72176b0573226
Rene Krajnc <renekrajnc@hotmail.com>
2023-02-08 23:23:42 +0100

    fonts, fontawesome, home btn, refactoring

--- portfolio/ui/layouts/FirstLayout/FirstLayout.tsx
+++ portfolio/ui/layouts/FirstLayout/FirstLayout.tsx
@@ -1,15 +1,22 @@
 import React, { FC } from 'react';
 import classes from './FirstLayout.module.scss';
-import bgImg from '../../../images/bg/photo-maldives.jpg';
+import bgImg from '../../../assets/images/bg/photo-maldives.jpg';
 import Navigation from '@/ui/components/Navigation/Navigation';
 import { menuItems, pages } from '@/constants/globalConstants';
 import { useRouter } from 'next/router';
+import { routes } from '@/constants/routes';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 interface FirstLayoutProps {
   page: React.ReactNode;
 }
 
 const FirstLayout: FC<FirstLayoutProps> = ({ page }) => {
+  console.log(faHouse);
+  const faPropIcon: IconProp = faHouse as IconProp;
   const router = useRouter();
   const currentPage = pages.find((page) => page.route === router.asPath);
   console.log(currentPage);
@@ -19,6 +26,9 @@ const FirstLayout: FC<FirstLayoutProps> = ({ page }) => {
       style={{ backgroundImage: `url('${bgImg.src}')` }}
     >
       <header>
+        <Link className={classes.home} href={routes.HOME}>
+          <FontAwesomeIcon icon={faPropIcon} />
+        </Link>
         <h1>{currentPage?.pageName}</h1>
         <Navigation navItems={menuItems} />
       </header>
