f1bf70b84afdfcb8de823d7d775b1610c1b9be1a
Rene Krajnc <renekrajnc@hotmail.com>
2026-02-07 23:55:19 +0100

    fix: hiding of fishing page
    - use getServerSideProps instead of getStaticProps for redirect from fishing page,
    - use sass map in mixins.scss

--- src/pages/fishing/index.tsx
+++ src/pages/fishing/index.tsx
@@ -8,20 +8,25 @@ const Fishing = () => {
 export default Fishing;
 
 // TODO: page is hidden for now and redirects to landing page. Update when ready
-export async function getStaticProps(
-  {
-    // locale
-  }: { locale: string },
-) {
+// export async function getStaticProps(
+//   {
+//     locale
+//   }: { locale: string },
+// ) {
+//   return {
+//     props: {
+//       ...(await serverSideTranslations(locale, ['common'])),
+//     },
+//   };
+// }
+
+export async function getServerSideProps() {
   return {
     redirect: {
       destination: Routes.LANDING_PAGE,
       permanent: false,
     },
     props: {},
-    // props: {
-    //   ...(await serverSideTranslations(locale, ['common'])),
-    // },
   };
 }
 
